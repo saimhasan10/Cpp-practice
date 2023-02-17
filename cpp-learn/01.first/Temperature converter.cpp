07426650471a4f42d876050ef38770be2f7c0310
saimhasan10 <saimhasan.nubt@gmail.com>
2023-02-17 16:41:18 +0600

    all temp

--- cpp-learn/01.first/Temperature converter.cpp
+++ cpp-learn/01.first/Temperature converter.cpp
@@ -1,13 +1,23 @@
+Tempeture conversions
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    double far,cels;
+    double far,cels,kel;
     cout<< "Enter celsius value: ";
     cin>> cels;
 
+    //celsius to Fahrenheit
     far =1.8*cels+32;
+
+    //Fahrenheit to celsius
+    //cels = (far-32)/1.8;
+
+    // celsius to kelvin
+    //kel=cels + 273;
+
 
     cout<<"Fahrenheit value is: "<<far<<endl;
     return 0;
