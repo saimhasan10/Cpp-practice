694d759484550b20b3a7a17f28b21a22813d2a56
saimhasan10 <saimhasan.nubt@gmail.com>
2023-02-17 16:33:25 +0600

    simple-temp

---
+++ cpp-learn/01.first/Temperature converter.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double far,cels;
+    cout<< "Enter celsius value: ";
+    cin>> cels;
+
+    far =1.8*cels+32;
+
+    cout<<"Fahrenheit value is: "<<far<<endl;
+    return 0;
+
+}
